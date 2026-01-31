49b9e2f1d0c9408679b67de08ec6b12111c9f6e6
Raven Espiritu <raven.espiritu@urios.edu.ph>
2026-01-31 09:16:35 +0800

    Allow Vercel domains in CORS

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,9 +15,12 @@ return [
     'allowed_origins' => [
         env('FRONTEND_URL', 'http://localhost:5173'),
         'http://localhost:3000',
+        'http://localhost:5173',
     ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^https://.*\.vercel\.app$#',
+    ],
 
     'allowed_headers' => ['*'],
 
