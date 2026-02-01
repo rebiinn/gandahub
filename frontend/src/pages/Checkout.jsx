cd39e26475de3207cbb422f16e3b5e92e9ee1942
Raven Espiritu <raven.espiritu@urios.edu.ph>
2026-02-01 18:47:59 +0800

    navicheckout

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { FaCreditCard, FaMoneyBill, FaMobile, FaCheckCircle } from 'react-icons/fa';
 import { useCart } from '../context/CartContext';
